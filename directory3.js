1405f587535d9083598851f83a2bb1d81dd8cf7c
Manas Chaturvedi <manas.oid@gmail.com>
2018-09-03 22:28:48 +0530

    'controller as' syntax example

---
+++ directory3.js
@@ -0,0 +1,14 @@
+angular.module('directoryApp',[])
+	.controller('directoryController',function()
+	{
+		var dirList = this;
+		dirList.list = [{'name':'Manas',age:25},{'name':'Pentagon Jr.','age':32},
+							{'name':'Santana',age:21}];
+
+		dirList.addPerson = function()
+		{
+			dirList.list.push({'name':dirList.name,'age':dirList.age});
+			dirList.name = '';
+			dirList.age = '';
+		}
+	});
